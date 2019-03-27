b2145c42718716907d1eb55c53b16d5ee45a828c
Manvi Tyagi <manvityagi770@gmail.com>
2019-03-28 03:43:56 +0530

    commiting styled auth

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+	'url' : '"mongodb://localhost/trial1db",{useNewUrlParser: true}' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
